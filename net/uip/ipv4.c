b3d7cd0539b3a5bb54622c3411035b06cb543dce
Arjan van de Ven <arjan@linux.intel.com>
2015-02-16 14:13:59 -0500

    Reduce console spam
    remove left over debug printf

--- net/uip/ipv4.c
+++ net/uip/ipv4.c
@@ -3,18 +3,9 @@
 int uip_tx_do_ipv4(struct uip_tx_arg *arg)
 {
 	struct uip_ip *ip;
-	int n;
-	uint8_t *p;
 
 	ip = (struct uip_ip *)(arg->eth);
 
-	p = (uint8_t *)arg->eth;
-
-	for (n = 0; n < 64; n++) {
-		printf("%02X ", *p++);
-		if ((n & 7) == 7)
-			printf("\n");
-	}
 	switch (ip->proto) {
 	case UIP_IP_P_ICMP:
 		uip_tx_do_ipv4_icmp(arg);
