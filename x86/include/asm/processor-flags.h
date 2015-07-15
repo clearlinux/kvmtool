7a5e69fb4d810ab678747aeebfd893c025091428
Dimitri John Ledkov <dimitri.j.ledkov@intel.com>
2015-07-15 12:51:53 +0100

    kvmtool: make vmlinux support patch compile in the stand alone kvmtool.

--- x86/include/asm/processor-flags.h
+++ x86/include/asm/processor-flags.h
@@ -7,4 +7,18 @@
  */
 #define X86_EFLAGS_CF	0x00000001 /* Carry Flag */
 
+/*
+ * Basic CPU control in CR0
+ */
+#define X86_CR0_PE_BIT		0 /* Protection Enable */
+#define X86_CR0_PE		_BITUL(X86_CR0_PE_BIT)
+#define X86_CR0_PG_BIT		31 /* Paging */
+#define X86_CR0_PG		_BITUL(X86_CR0_PG_BIT)
+
+/*
+ * Intel CPU features in CR4
+ */
+#define X86_CR4_PAE_BIT		5 /* enable physical address extensions */
+#define X86_CR4_PAE		_BITUL(X86_CR4_PAE_BIT)
+
 #endif
