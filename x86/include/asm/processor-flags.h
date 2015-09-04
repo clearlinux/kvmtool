e0b5a5d99cecbdb73b0016d8cf7bdba39c8cc1a7
Dimitri John Ledkov <dimitri.j.ledkov@intel.com>
2015-09-04 16:06:53 +0100

    Fix compilation with old kernel headers.
    Signed-off-by: Dimitri John Ledkov <dimitri.j.ledkov@intel.com>

--- x86/include/asm/processor-flags.h
+++ x86/include/asm/processor-flags.h
@@ -2,6 +2,22 @@
 #define _ASM_X86_PROCESSOR_FLAGS_H
 /* Various flags defined: can be included from assembler. */
 
+#ifndef _BITUL
+
+#ifdef __ASSEMBLY__
+#define _AC(X,Y)	X
+#define _AT(T,X)	X
+#else
+#define __AC(X,Y)	(X##Y)
+#define _AC(X,Y)	__AC(X,Y)
+#define _AT(T,X)	((T)(X))
+#endif
+
+#define _BITUL(x)	(_AC(1,UL) << (x))
+#define _BITULL(x)	(_AC(1,ULL) << (x))
+
+#endif
+
 /*
  * EFLAGS bits
  */
